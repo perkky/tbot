29d69cb34fcae745e00d4522788c045b1a4d4c3d
Nhan Dao <nhan.dao@Outlook.com>
2018-04-07 01:44:27 +0800

    Fully implemented BitfinexReader using regex.

--- BitfinexReader.py
+++ BitfinexReader.py
@@ -2,28 +2,36 @@
 import urllib2
 import re
 
-def fetchData(url):
-    response = urllib2.urlopen(url)
-    html = response.read() #Raw HTML data
-    html = html[1:len(html) - 1]
-    data = re.findall(r'\[(\d{13}),((?:\d{4}.[0-9]+)|(?:\d{4})),((?:\d{4}.[0-9]+)|(?:\d{4})),((?:\d{4}.[0-9]+)|(?:\d{4})),((?:\d{4}.[0-9]+)|(?:\d{4})),((?:\d{3}.[0-9]+)|(?:\d{4}))],?',html)
-    data = list(data)
+def fetchData(timeFrame, coinCode, outputFile, targetLength):
+    regex = re.compile(r"\d{13},(?:(?:(?:\d{4}\.[0-9]+)|(?:\d{4})),){4}[0-9]+\.[0-9]+")
+    data = []
+    length = 0
+
+    while length < targetLength:
+        urlPost = ""
+        url = 'https://api.bitfinex.com/v2/candles/trade:{x}:{y}/hist?limit=500{z}'.format(x=timeFrame, y=coinCode, z=urlPost)
+
+        response = urllib2.urlopen(url)
+        html = response.read()
+        rawData = regex.findall(html)
+        tmpData = [item.split(",") for item in rawData]
+        urlPost = "&end={MTS}".format(MTS=str(tmpData.pop(len(tmpData)-1))[0])
+        data.append(tmpData)
+
+        length = length + len(tmpData)
 
     return data
+
+def writeToFile(data):
+    f = open(outputFile, 'a')
+    for entry in data:
+        for item in entry:
+            f.write("{MTS},{OPEN},{CLOSE},{HIGH},{LOW},{VOLUME}\n".format(MTS=item[0],OPEN=item[1],CLOSE=item[2],HIGH=item[3],LOW=item[4],VOLUME=item[5]))
 
 
 timeFrame = "15m"
 coinCode = "tBTCUSD"
 outputFile = "5 Bitfinex 15m data.txt"
-targetDataSet = 5000
-
-url = 'https://api.bitfinex.com/v2/candles/trade:{x}:{y}/hist?limit=1000'.format(x=timeFrame, y=coinCode)
-
-
-f = open(outputFile, 'w')
-response = urllib2.urlopen(url)
-html = response.read() #Raw HTML data
-html = html[1:len(html) - 1]
-data = fetchData(url)
-for entry in data:
-    f.write(str(entry) + "\n")
+targetLength = 1
+#fetchData(timeFrame, coinCode, outputFile, targetLength)
+writeToFile(fetchData(timeFrame, coinCode, outputFile, targetLength))

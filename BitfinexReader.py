32c5bba9f76d22f3cacac9f0a9801807a0adb917
Nhan Dao <nhan.dao@Outlook.com>
2018-04-07 13:19:09 +0800

    Clean up and bug fixes on BitfinexReader: - Regex is now shorter and more concise - Fixed** Regex was not reading other coins properly - Fixed** Parse the same timeFrame mutliple times from Bitfinex - Can now send request indefinitely until number of entries reach the target

--- BitfinexReader.py
+++ BitfinexReader.py
@@ -1,36 +1,44 @@
-#Reads data from the bitfinex website and output it to a csv file
+#Author  : Nhan Dao
+#Purpose : Extract HTML from "Bitfinex' Trade Candle" cUrl, format the data and output to a specify file.
+#          Multiple html request may be sent until enough data is extracted.
+
 import urllib2
 import re
+import time
 
 def fetchData(timeFrame, coinCode, outputFile, targetLength):
-    regex = re.compile(r"\d{13},(?:(?:(?:\d{4}\.[0-9]+)|(?:\d{4})),){4}[0-9]+\.[0-9]+")
+    regex = re.compile(r"\[([0-9]+,(?:[0-9]+\.?[0-9]+,?){5})]") #Regex for [[MTS,OPEN,CLOSE,HIGH,LOW,VOLUME],...]
     data = []
-    length = 0
+    numEntries = 0
+    urlPost = ""
 
-    while length < targetLength:
-        urlPost = ""
+    while numEntries < targetLength:
         url = 'https://api.bitfinex.com/v2/candles/trade:{x}:{y}/hist?limit=1000{z}'.format(x=timeFrame, y=coinCode, z=urlPost)
 
-        response = urllib2.urlopen(url)
-        html = response.read()
-        rawData = regex.findall(html)
-        tmpData = [item.split(",") for item in rawData]
-        urlPost = "&end={MTS}".format(MTS=str(tmpData.pop(len(tmpData)-1))[0])
-        data.append(tmpData)
+        try:
+            response = urllib2.urlopen(url)
+            html = response.read()
+            rawData = regex.findall(html)
+            tmpData = [item.split(",") for item in rawData]
+            urlPost = "&end={MTS}".format(MTS=tmpData.pop(len(tmpData) - 1)[0]) #Last entry is deleted, otherwise it will be repeated.
+            data.append(tmpData)
+            numEntries += len(tmpData)
 
-        length = length + len(tmpData)
+        except urllib2.HTTPError as err:
+            print err
+            print "Trying again in 60 seconds"
+            time.sleep(60)
+
+    print "Successfully fetch data, {x} number of entries.".format(x=numEntries)
 
     return data
 
-def writeToFile(data):
+def writeToFile(data, outputFile):
     f = open(outputFile, 'a')
-    for entry in data:
-        for item in entry:
-            f.write("{MTS},{OPEN},{CLOSE},{HIGH},{LOW},{VOLUME}\n".format(MTS=item[0],OPEN=item[1],CLOSE=item[2],HIGH=item[3],LOW=item[4],VOLUME=item[5]))
 
 
 timeFrame = "15m"
 coinCode = "tBTCUSD"
 outputFile = "5 Bitfinex 15m data.txt"
-targetLength = 10000
-writeToFile(fetchData(timeFrame, coinCode, outputFile, targetLength))
+targetLength = 20000
+writeToFile(fetchData(timeFrame, coinCode, outputFile, targetLength), outputFile)

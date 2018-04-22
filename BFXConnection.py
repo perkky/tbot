97305b06f002856e47dbee58ac975f1b7d70b2b9
ryanpyrc <ryan.pyrc@hotmail.com>
2018-04-23 00:53:11 +0800

    Added a getResponse() function, still needs work

--- BFXConnection.py
+++ BFXConnection.py
@@ -14,6 +14,8 @@ class BFXConnection:
     def _authSig(self, secret):
         return hmac.new(secret, msg=self._authPayload(),digestmod=hashlib.sha384).hexdigest()
     def __init__(self, url, key, secret):
+        self.on = True
+
         self.ws = create_connection(url)
 
         authNonce = self._nonce()
@@ -24,6 +26,72 @@ class BFXConnection:
         self.ws.send(msg)
 
         result = self.ws.recv()
-        print "Recieved %s" % result
-        result = self.ws.recv()
-        print "Recieved %s" % result
+        jsonData = json.loads(result)
+
+        while not jsonData['event'] == "auth":
+            result = self.ws.recv()
+            jsonData = json.loads(result)
+
+        if jsonData['status'] == "OK":
+            print result
+            print "Connection successful!"
+        else:
+            print "Could not connect."
+
+    def getPositions(self):
+        #nothing, get rid when finished
+        #msg = json.dumps({ 'event':'subscribe', 'channel': "Candles", 'key': 'trade:15m:tBTCUSD'})
+        msg = json.dumps({ 'event':'subscribe', 'channel': "positions"})
+        self.ws.send(msg)
+
+    def getResponse(self):
+        #waits for the response and then updates the needed fields
+        #codes are in the codes.txt file
+        response = self.ws.recv()
+        jsonData = json.loads(response)
+
+        #checks that the server is connected\
+        #make sure it closes all positions before it shutsoff
+        try:
+            if jsonData['status'] == "FAIL":
+                print "Connection has fail"
+                print "Bot shutting off..."
+                self.on = False
+            #checks the error codes
+        except TypeError as err:
+            #do nothing
+            pass
+        except KeyError as err:
+            #do nothing
+            pass
+
+        #check for error codes
+        try:
+            if jsonData['code'] == 10300:
+                print "Message was wrong"
+            elif jsonData['code'] == 10000:
+                print "Unkown Error"
+                print "Bot shutting off..."
+                self.on = false
+            #checks the error codes
+        except TypeError as err:
+            #do nothing
+            pass
+        except KeyError as err:
+            #do nothing
+            pass
+
+        #check for update from server
+        try:
+            if jsonData[1] =="ps":
+                print jsonData[2]
+            elif jsonData[1] =="ws":
+                print jsonData[2]
+        except KeyError as err:
+            #do nothing
+            pass
+
+
+con.getPositions()
+while True:
+    con.getResponse()

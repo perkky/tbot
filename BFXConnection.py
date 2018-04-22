b3fc760098441dcb9324607381842ec000e843e4
ryanpyrc <ryan.pyrc@hotmail.com>
2018-04-22 23:09:44 +0800

    Updated bfxconnection so it actually works

--- BFXConnection.py
+++ BFXConnection.py
@@ -7,11 +7,18 @@ import os
 import time
 
 class BFXConnection:
+    def _nonce(self):
+        return str(int(round(time.time()*10000)))
+    def _authPayload(self):
+        return "AUTH" + str(self._nonce())
+    def _authSig(self, secret):
+        return hmac.new(secret, msg=self._authPayload(),digestmod=hashlib.sha384).hexdigest()
     def __init__(self, url, key, secret):
         self.ws = create_connection(url)
+
         authNonce = self._nonce()
-        authPayload = "AUTH" + str(authNonce)
-        authSig = hmac.new(secret, msg=authPayload,digestmod=hashlib.sha384).hexdigest()
+        authPayload = self._authPayload()
+        authSig = self._authSig(secret)
 
         msg = json.dumps( {"apiKey": key, "authSig": authSig, "authNonce": authNonce, "authPayload": authPayload, 'event': 'auth'}, separators=(',',':') )
         self.ws.send(msg)
@@ -21,8 +28,7 @@ class BFXConnection:
         result = self.ws.recv()
         print "Recieved %s" % result
 
-    def _nonce(self):
-        return str(int(round(time.time()*10000)))
+
 
 con = BFXConnection("wss://api.bitfinex.com/ws/2", "ykVwqMBQOZJXwxfI0Qb4cGJVEa446ra83dWzJmsCt4d", "wm9oagLDmutvDlu2Ka9cWPtW7k5g8NLzv146LS28VgI")
 

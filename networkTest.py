a6ba5569c38030303a8c3f3646a28b266c9dc6d0
ryanpyrc <ryan.pyrc@hotmail.com>
2018-04-27 11:21:19 +0800

    updated nt

--- networkTest.py
+++ networkTest.py
@@ -1,4 +1,24 @@
 import FinexAPI
+import json
+from datetime import datetime
+import time
 
-print FinexAPI.active_positions()
-print FinexAPI.close_position(136564986)
+print FinexAPI.balances()
+print str(datetime.now().minute*60 + datetime.now().second)
+print str(datetime.now().second)
+
+while True:
+    time.sleep(2)
+    seconds = datetime.now().second
+    time.sleep(60-seconds)
+    print "poo"
+
+"""
+result = str(FinexAPI.active_positions()[0])
+
+result = json.loads(makeJSONReadable(result))
+
+print result['id']
+new = FinexAPI.close_position(result['id'])
+new = json.loads(makeJSONReadable(str(new)))
+"""
